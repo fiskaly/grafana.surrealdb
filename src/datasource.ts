82f6a43ec20e4f176a101956c4530b7e7d621ce1
Philipp Paulweber <ppaulweber@fiskaly.com>
2024-01-19 15:34:54 +0100

    Datasource: clean-up of frontend by removing yet not needed 'getResource' requests

--- src/datasource.ts
+++ src/datasource.ts
@@ -23,25 +23,22 @@ import
 export class DataSource extends DataSourceWithBackend<MyQuery, MyDataSourceOptions> {
     constructor(instanceSettings: DataSourceInstanceSettings<MyDataSourceOptions>) {
 	super(instanceSettings);
-	this.getResource('constructor');
     }
 
     getDefaultQuery(coreApp: CoreApp): Partial<MyQuery> {
-	this.getResource('getDefaultQuery');
 	return DEFAULT_QUERY;
     }
 
     query(request: DataQueryRequest<MyQuery>): Observable<DataQueryResponse> {
-	this.getResource('query');
 	return super.query(request);
     }
 
     // https://grafana.com/docs/grafana/latest/dashboards/variables/add-template-variables/#global-variables
-    applyTemplateVariables(query: MyQuery) {//, scopedVars: ScopedVars)) {
+    applyTemplateVariables(query: MyQuery) {
 	const templateSrv = getTemplateSrv();
 	return {
 	    ...query,
-	    surql: templateSrv.replace(query.surql),//, scopedVars),
+	    surql: templateSrv.replace(query.surql),
 	};
     }
 }
